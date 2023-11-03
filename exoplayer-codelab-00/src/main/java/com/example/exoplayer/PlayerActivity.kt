0c36a6f280a8ff06485a273027b241b831d0d1c2
karshC <karshpsu@gmail.com>
2023-11-03 13:43:00 -0400

    setting player lifecycle based on activity lifecycle

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -15,9 +15,15 @@
  */
 package com.example.exoplayer
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.ExoPlayer
 import com.example.exoplayer.databinding.ActivityPlayerBinding
 
@@ -30,6 +36,9 @@ class PlayerActivity : AppCompatActivity() {
         ActivityPlayerBinding.inflate(layoutInflater)
     }
     private var player: ExoPlayer? = null
+    private var playWhenReady = true
+    private var mediaItemIndex = 0
+    private var playbackPosition = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +51,49 @@ class PlayerActivity : AppCompatActivity() {
             .also { exoPlayer ->
                 viewBinding.videoView.player = exoPlayer
                 val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
-                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.setMediaItems(listOf(mediaItem), mediaItemIndex, playbackPosition)
+                exoPlayer.playWhenReady
+                exoPlayer.prepare()
             }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if(Build.VERSION.SDK_INT > 23)initializePlayer()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if(Build.VERSION.SDK_INT <= 23 || player == null)initializePlayer()
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Build.VERSION.SDK_INT <= 23)releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(Build.VERSION.SDK_INT > 23)releasePlayer()
+    }
+
+    private fun releasePlayer() {
+        player?.let{ exoplayer ->
+            playbackPosition = exoplayer.currentPosition
+            mediaItemIndex = exoplayer.currentMediaItemIndex
+            playWhenReady = exoplayer.playWhenReady
+            exoplayer.release()
+        }
+        player = null
     }
 }
